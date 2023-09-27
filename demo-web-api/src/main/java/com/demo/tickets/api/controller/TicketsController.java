9f1e72d222c1ba4e946d16875c4d4cf55108b126
longdeptrai09 <tranhoanglongdeptrai09@gmail.com>
2023-09-27 12:05:47 +0700

    12:05 27/09/23

---
+++ demo-web-api/src/main/java/com/demo/tickets/api/controller/TicketsController.java
@@ -0,0 +1,15 @@
+package com.demo.tickets.api.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/")
+public class TicketsController {
+
+//    @GetMapping("")
+
+}
