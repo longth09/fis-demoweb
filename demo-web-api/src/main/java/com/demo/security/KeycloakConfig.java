7068dba7de9cc7edaf51090df35ca5119a23b683
longdeptrai09 <tranhoanglongdeptrai09@gmail.com>
2023-10-06 17:13:04 +0700

    06/10/23

--- demo-web-api/src/main/java/com/demo/security/KeycloakConfig.java
+++ demo-web-api/src/main/java/com/demo/security/KeycloakConfig.java
@@ -1,22 +1,26 @@
 package com.demo.security;
 
+import org.keycloak.adapters.KeycloakConfigResolver;
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.KeycloakSecurityComponents;
 import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
-import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-
-import java.lang.reflect.Method;
 
 
+@Configuration
+@EnableWebSecurity
+@ComponentScan(basePackageClasses = KeycloakSecurityComponents.class)
 public class KeycloakConfig extends KeycloakWebSecurityConfigurerAdapter {
 
     /**
@@ -24,62 +28,32 @@ public class KeycloakConfig extends KeycloakWebSecurityConfigurerAdapter {
      */
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        KeycloakAuthenticationProvider keycloakAuthenticationProvider = new KeycloakAuthenticationProvider();
+        KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
         keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
-
         auth.authenticationProvider(keycloakAuthenticationProvider);
     }
 
+    @Bean
+    public KeycloakConfigResolver KeycloakConfigResolver() {
+        return new KeycloakSpringBootConfigResolver();
+    }
+
+    /**
+     * Defines the session authentication strategy.
+     */
+    @Bean
     @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring()
-                .antMatchers(
-                        // Client-side JS
-                        // TODO
-                        // "/**",
-
-                        // the standard favicon URI
-                        "/favicon.ico",
-
-                        // the robots exclusion standard
-                        "/robots.txt",
-
-                        // web application manifest
-                        "/manifest.webmanifest", "/sw.js", "/offline.html",
-
-                        // icons and images
-                        "/icons/**", "/images/**", "/styles/**", "/css/**", "/js/**",
-                        "/sw-runtime-resources-precache.js");
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
         http
                 .authorizeRequests()
-                .antMatchers("/accounts").permitAll() // Chỉ cho người dùng đã xác thực
-                .anyRequest().authenticated() // Cho phép tất cả mọi người
-                .and().oauth2Login()
-                .and()
-                .logout()
-                .invalidateHttpSession(true)
-                .clearAuthentication(true)
-                .logoutSuccessUrl("/login?logout")
-                .deleteCookies("JSESSIONID")
-                .permitAll()
-                .and()
-                .csrf()
-                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
-    }
-
-
-    @Override
-    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
-        return new RegisterSessionAuthenticationStrategy(buildSessionRegistry());
-    }
-
-    @Bean
-    protected SessionRegistry buildSessionRegistry() {
-        return new SessionRegistryImpl();
+                .antMatchers("/accounts").hasRole("admin")
+                .anyRequest().permitAll();
     }
 
 }
